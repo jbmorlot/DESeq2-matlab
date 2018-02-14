26b71cabbe2a2899bf401ca62d398002ef0dde75
jbmorlot <jb.morlot@gmail.com>
2018-02-14 16:08:42 +0100

    Correction & Readme

--- DESeq2.m
+++ DESeq2.m
@@ -1,8 +1,11 @@
-
-function [log2FC,FDR,meanExp] = DESeq2(matrix1,matrix2):
-% matrix1: matrix where each line represent a sample in the condition 1
-% matrix2: matrix where each line represent a sample in the condition 1
+function [log2FC,FDR,meanExp] = DESeq2(matrix1,matrix2)
+% matrix1: matrix where each line represent a sample in the condition 1 in the
+% format samples x genes
+% matrix2: matrix where each line represent a sample in the condition 2 in the
+% format samples x genes
 %
+% The overexpressed genes are those with a FDR<0.1 and log2FC>1 and under-expressed
+%  with a FDR<0.1 and log2FC<-1
 %
 
 disp('Normalization ...')
@@ -21,21 +24,21 @@ matrixN2 = bsxfun(@rdivide,matrix2',norm2')';
 disp('Log2FC(mean)...')
 mean1 = mean(matrixN1);
 mean2 = mean(matrixN2);
-meanExp = mean(cat(2,matrix1,matrix2))
+meanExp = mean(cat(1,matrix1,matrix2));
 
 log2FC = log2(mean1./mean2)';
 
 disp('Differential Analysis ...')
 tLocal = nbintest(matrix1',matrix2','VarianceLink','LocalRegression','SizeFactor',{norm1',norm2'});
-FDR = mafdr(tLocal_ij.pValue,'BHFDR',true);
+FDR = mafdr(tLocal.pValue,'BHFDR',true);
 
 
 figure;
-scatter(meanExp(FDR<0.1),log2FC(FDR<0.1),2,'r','filled');
+scatter(meanExp(FDR<0.1),log2FC(FDR<0.1),5,'r','filled');
 hold on;
-scatter(meanExp(FDR>=0.1),log2FC(FDR>=0.1),2,'k','filled')
+scatter(meanExp(FDR>=0.1),log2FC(FDR>=0.1),5,'k','filled')
 set(gca, 'XScale', 'log')
-xtitle('Mean expression')
-ytitle('Log2 fold change')
+xlabel('Mean expression')
+ylabel('Log2 fold change')
 
 end
